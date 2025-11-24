cdcd4f29a292f09ff01e0afd5314665fe3dee182
junPark <iii211sdf@naver.com>
2025-11-24 23:05:20 +0900

    Update

---
+++ WpfExplorer/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer
+{
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new Window();
+        }
+    }
+}
