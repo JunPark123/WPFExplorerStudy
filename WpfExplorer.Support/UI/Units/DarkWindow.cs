30ccbd196cb7ed4b27f427bd1341352a3f75204a
junPark <iii211sdf@naver.com>
2025-12-05 23:43:52 +0900

    상단바 삭제. DarkWindow에 Location 등록을 위한 DP 생성 Explorer에서 영역을 나누도록 수정

--- WpfExplorer.Support/UI/Units/DarkWindow.cs
+++ WpfExplorer.Support/UI/Units/DarkWindow.cs
@@ -5,10 +5,42 @@ namespace WpfExplorer.Support.UI.Units
 {
     public class DarkWindow : JamesWindow
     {
+        public static readonly DependencyProperty LocationTemplateProperty =
+            DependencyProperty.Register("LocationTemplate",
+                typeof(DataTemplate),
+                typeof(DarkWindow),
+                new PropertyMetadata(null));
+
+        public DataTemplate LocationTemplate
+        {
+            get { return (DataTemplate)GetValue(LocationTemplateProperty); }
+            set { SetValue(LocationTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty LocationProperty =
+            DependencyProperty.Register("Location", 
+                typeof(object),
+                typeof(DarkWindow),
+                new UIPropertyMetadata(null));
+
+        public object Location
+        {
+            get { return GetValue(LocationTemplateProperty); }
+            set
+            {
+                SetValue(LocationTemplateProperty, value);
+            }
+        }
+
         static DarkWindow()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), 
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow),
                 new FrameworkPropertyMetadata(typeof(DarkWindow)));
         }
+
+
+        
+
+
     }
 }
